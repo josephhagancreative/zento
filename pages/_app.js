8cd5090259c819bdaa7da69903d99a5ac2e33af0
Joe Hagan <jhaganfilm@gmail.com>
2022-05-07 16:36:00 +0900

    Trying to load a script

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import Layout from "../comps/Layout"
 import "../styles/globals.css"
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1842241795685614"
+        crossorigin="anonymous"
+        strategy="beforeInteractive"></Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
